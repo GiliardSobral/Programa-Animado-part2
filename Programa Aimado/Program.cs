74b396c0f436e02b64a34159c3a3c95bcfdac20a
Giliard <giliardpreirasobral@hotmail.com>
2019-01-28 16:22:59 -0300

    Projeto iniciado parte 2

--- Programa Aimado/Program.cs
+++ Programa Aimado/Program.cs
@@ -10,6 +10,47 @@ namespace Programa_Aimado
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.Write("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Bem Vindo ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine("╔═════════════════MENU DE OPÇÕES════════════════╗    ");
+
+            Console.WriteLine("║ 1 Calculadora                         ║    ");
+
+            Console.WriteLine("║                                               ║    ");
+
+            Console.WriteLine("║ 2 Jogo Xadrez                                     ║    ");
+
+            Console.WriteLine("║                                               ║    ");
+
+            Console.WriteLine("║ 3 Escola                          ║    ");
+
+            Console.WriteLine("║                                               ║    ");
+
+            Console.WriteLine("║ 4 BIBLIOTECA                                  ║    ");
+
+            Console.WriteLine("║                                               ║    ");
+
+            Console.WriteLine("║ 5 CONFIGURAÇÃO                                ║    ");
+
+            Console.WriteLine("║                                               ║    ");
+
+            Console.WriteLine("║ 6 SAIR                                        ║    ");
+
+            Console.WriteLine("╚═══════════════════════════════════════════════╝    ");
+
+            Console.WriteLine(" ");
+
+
+            Console.Write("DIGITE UMA OPÇÃO : ");
+            Console.ReadKey();
+
+
+
         }
     }
 }
